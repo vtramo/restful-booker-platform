117391f85865dd2dcf8fbb0c64868bfaa83e0390
Vincenzo Tramo <vv.tramo@gmail.com>
2023-11-05 20:57:05 +0100

    fix: use docker compose for run locally

--- run_locally.sh
+++ run_locally.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 
+docker_compose_down () {
+  docker compose down
+}
+
 printf "\n####### RESTFUL-BOOKER-PLATFORM #######
 ####                               ####
 ####    STARTING APPLICATION...    ####
 ####                               ####
 #######################################\n\n"
 
-trap "kill 0" EXIT
+docker compose -f ./docker-compose.yml up -d
 
-java -jar -Dspring.profiles.active=dev auth/target/restful-booker-platform-auth-*.jar > auth.log &
-java -jar -Dspring.profiles.active=dev booking/target/restful-booker-platform-booking-*.jar > booking.log &
-java -jar -Dspring.profiles.active=dev room/target/restful-booker-platform-room-*.jar > room.log &
-java -jar -Dspring.profiles.active=dev report/target/restful-booker-platform-report-*.jar > report.log &
-java -jar -Dspring.profiles.active=dev branding/target/restful-booker-platform-branding-*.jar > branding.log &
-java -jar -Dspring.profiles.active=dev message/target/restful-booker-platform-message-*.jar > message.log &
-java -jar -Dspring.profiles.active=dev assets/api/target/restful-booker-platform-assets-*.jar > ui.log &
-java -jar proxy/target/restful-booker-platform-local-proxy-*.jar > proxy.log &
-
-node .utilities/monitor/local_monitor.js
+node .utilities/local_monitor.js
 
 printf "\n####### RESTFUL-BOOKER-PLATFORM #######
 ####                               ####
@@ -28,4 +23,6 @@ printf "\n####### RESTFUL-BOOKER-PLATFORM #######
 ####                               ####
 #######################################"
 
-wait
+trap docker_compose_down INT
+
+sleep 999d
